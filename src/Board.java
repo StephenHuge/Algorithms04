d7f1695cf79595c5f4b3fbea84023d6a2d871e8f
HJS <407410014@qq.com>
2017-10-27 22:08:00 +0800

    finish hamming() and manhattan()

--- src/Board.java
+++ src/Board.java
@@ -2,39 +2,64 @@ import edu.princeton.cs.algs4.In;
 
 public class Board {
     private final int[][] blockz;
-    // construct a board from an n-by-n array of blocks
-    // (where blocks[i][j] = block in row i, column j)
-    public Board(int[][] blocks)           
+
+    private final int[][] goal;
+
+    public Board(int[][] blocks)    // construct a board from an n-by-n array of blocks           
     {
         validate(blocks);
         blockz = new int[blocks.length][blocks[0].length];
+        int num  = 0;
+        goal = new int[blocks.length][blocks[0].length];
+
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[0].length; j++) {
                 blockz[i][j] = blocks[i][j];
             }
         }
-    }
-    private void validate(int[][] blocks) {
-        if (blocks == null)
-            throw new java.lang.IllegalArgumentException();
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[0].length; j++) {
-                if (blocks[i][j] < 0)
-                    throw new java.lang.IllegalArgumentException();
+                if (i == blocks.length - 1 && j == blocks.length - 1)   break;
+                goal[i][j] = ++num;
             }
         }
     }
+
     public int dimension()                 // board dimension n
     {
         return blockz.length;
     }
     public int hamming()                   // number of blocks out of place
     {
-        return -1;
+        int hamming = 0;
+        for (int i = 0; i < blockz.length; i++) {
+            for (int j = 0; j < blockz[0].length; j++) {
+                if (blockz[i][j] != 0 && blockz[i][j] != goal[i][j])    hamming++;
+            }
+        }
+        return hamming;
     }
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
     {
-        return -1;
+        int manhattan = 0;
+        for (int i = 0; i < blockz.length; i++) {
+            for (int j = 0; j < blockz[0].length; j++) {
+                if (blockz[i][j] != 0) {
+
+                    // e.g.: 5 is in (0, 0)
+                    // 5 - 1 = 4, x = 4 / 3 - 0 = 1, y = 4 % 3 - 0 = 1
+                    // so manhattan distance of 5 is 1 + 1 = 2
+                    int node = blockz[i][j] - 1;
+                    int x = node / 3 - i;        
+                    int y = node % 3 - j;
+
+                    int xDistance = x > 0 ? x : -x;     // make sure x distance is positive
+                    int yDistance = y > 0 ? y : -y;     // make sure y distance is positive
+                    manhattan += xDistance + yDistance;
+                }
+            }
+        }
+        return manhattan;
     }
     public boolean isGoal()                // is this board the goal board?
     {
@@ -64,7 +89,16 @@ public class Board {
         }
         return new String(sb);
     }
-
+    private void validate(int[][] blocks) {
+        if (blocks == null)
+            throw new java.lang.IllegalArgumentException();
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[0].length; j++) {
+                if (blocks[i][j] < 0)
+                    throw new java.lang.IllegalArgumentException();
+            }
+        }
+    }
     public static void main(String[] args) // unit tests (not graded)
     {
         In in = new In(args[0]);
@@ -85,6 +119,9 @@ public class Board {
         //            }
         //            System.out.println();
         //        }
-        System.out.println(board);
+        //        System.out.println(board);
+
+        System.out.println("Hamming solution is " + board.hamming() + 
+                "\nManhattan solution is " + board.manhattan());
     }
 }
