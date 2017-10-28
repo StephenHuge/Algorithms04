0f18463203566fea871f96159a1e6dbf4da26257
HJS <407410014@qq.com>
2017-10-28 19:10:58 +0800

    update 2-dim integer arrays to 1-dim char arrays

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,9 @@
 import edu.princeton.cs.algs4.In;
 
 public class Board {
-    private final int[][] blockz;
+    private final char[] blockz;
 
-    private final int[][] goal;
+    private final char[] goal;
 
     private final int n; 
 
@@ -13,13 +13,13 @@ public class Board {
     {
         validate(blocks);
         n = blocks.length;
-        blockz = new int[n][n];
+        blockz = new char[n * n];
         int num  = 0;
-        goal = new int[n][n];
+        goal = new char[n * n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                blockz[i][j] = blocks[i][j];
+                blockz[i * n + j] = (char) (blocks[i][j] + 48);
             }
         }
 
@@ -29,7 +29,7 @@ public class Board {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == n - 1 && j == n - 1)   break;
-                goal[i][j] = ++num;
+                goal[i * n + j] = (char) (++num + 48);
             }
         }
     }
@@ -44,7 +44,7 @@ public class Board {
         int hamming = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (blockz[i][j] != 0 && blockz[i][j] != goal[i][j])    hamming++;
+                if (blockz[i * n + j] != '0' && blockz[i * n + j] != goal[i * n + j])    hamming++;
             }
         }
         return hamming;
@@ -53,29 +53,6 @@ public class Board {
     {
         return manhattan;
     }
-    private int getManhattan() {
-        if (isGoal())   return 0;
-        int mManhattan = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (blockz[i][j] != 0) {
-
-                    // e.g.: 5 is in (0, 0)
-                    // 5 - 1 = 4, x = 4 / 3 - 0 = 1, y = 4 % 3 - 0 = 1
-                    // so manhattan distance of 5 is 1 + 1 = 2
-                    int node = blockz[i][j] - 1;
-                    int x = node / 3 - i;        
-                    int y = node % 3 - j;
-
-                    int xDistance = x > 0 ? x : -x;     // make sure x distance is positive
-                    int yDistance = y > 0 ? y : -y;     // make sure y distance is positive
-                    mManhattan += xDistance + yDistance;
-                }
-            }
-        }
-        return mManhattan;
-    }
-
     public boolean isGoal()                // is this board the goal board?
     {
         return isArrayEqual(blockz, goal);
@@ -93,18 +70,6 @@ public class Board {
         Board b = (Board) y;
         return isArrayEqual(this.blockz, b.blockz);
     }
-    /**
-     * this method is just for verifying whether 2 arrays is equal, we **don't** offer validation
-     * for these two's length
-     */
-    private boolean isArrayEqual(int[][] a, int[][] b) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][j] != b[i][j])    return false;
-            }
-        }
-        return true;
-    }
     public Iterable<Board> neighbors()     // all neighboring boards
     {
         return null;
@@ -115,7 +80,7 @@ public class Board {
         s.append(n + "\n");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                s.append(String.format("%2d ", blockz[i][j]));
+                s.append(String.format("%2c ", blockz[i * n + j]));
             }
             s.append("\n");
         }
@@ -131,30 +96,64 @@ public class Board {
             }
         }
     }
+    private int getManhattan() {
+        if (isGoal())   return 0;
+        int mManhattan = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blockz[i * n + j] != '0') {
+
+                    // e.g.: 5 is in (0, 0)
+                    // 5 - 1 = 4, x = 4 / 3 - 0 = 1, y = 4 % 3 - 0 = 1
+                    // so manhattan distance of 5 is 1 + 1 = 2
+                    int node = blockz[i * n + j] - 48 - 1;
+                    int x = node / 3 - i;        
+                    int y = node % 3 - j;
+
+                    int xDistance = x > 0 ? x : -x;     // make sure x distance is positive
+                    int yDistance = y > 0 ? y : -y;     // make sure y distance is positive
+                    mManhattan += xDistance + yDistance;
+                }
+            }
+        }
+        return mManhattan;
+    }
+    /**
+     * this method is just for verifying whether 2 arrays is equal, we **don't** offer validation
+     * for these two's length
+     */
+    private boolean isArrayEqual(char[] a, char[] b) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i * n + j] != b[i * n + j])    return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) // unit tests (not graded)
     {
         In in = new In(args[0]);
         int n = in.readInt();
         int[][] blocks = new int[n][n];
 
+                for (int i = 0; i < blocks.length; i++) {
+                    for (int j = 0; j < blocks[0].length; j++) {
+                        blocks[i][j] = in.readInt();
+//                                        System.out.print(blocks[i][j] + "\t");
+                    }
+//                                System.out.println();
+                }
+        Board board = new Board(blocks);
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[0].length; j++) {
-                blocks[i][j] = in.readInt();
-                //                System.out.print(blocks[i][j] + "\t");
+                System.out.print(board.blockz[i * n + j] + "\t");
             }
-            //            System.out.println();
+            System.out.println();
         }
-        Board board = new Board(blocks);
-        //        for (int i = 0; i < blocks.length; i++) {
-        //            for (int j = 0; j < blocks[0].length; j++) {
-        //                System.out.print(board.blockz[i][j] + "\t");
-        //            }
-        //            System.out.println();
-        //        }
-        System.out.println(board);
-
-        System.out.println("Hamming solution is " + board.hamming() + 
-                "\nManhattan solution is " + board.manhattan());
-        System.out.println("This array is goal? " + board.isGoal());
+                System.out.println(board);
+        
+                System.out.println("Hamming solution is " + board.hamming() + 
+                        "\nManhattan solution is " + board.manhattan());
+                System.out.println("This array is goal? " + board.isGoal());
     }
 }
